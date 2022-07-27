0b2a894be8c2a443941645c726a6734f578c0414
Nicolas D. Enrique <nicolas.enrique56@gmail.com>
2022-07-27 08:08:32 -0300

    form validations

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,42 @@
 import React from 'react';
 
 const Login = () => {
+	const submitHandler = (e) => {
+		e.preventDefault();
+		const email = e.target.email.value;
+		const password = e.target.password.value;
+
+		const regexEmail =
+			/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
+
+		
+		if (email === '' || password === '') {
+			alert('Please fill in all fields');
+            return;
+		}
+
+		if (email !== '' && !regexEmail.test(email)) {
+			alert('Please enter a valid email');
+            return;
+		}
+
+        if (email !== 'challenge@alkemy.org' || password !== 'react') {
+            alert('Invalid email or password');
+            return;
+        }
+        console.log('Ok to log')
+	};
+
+
+
 	return (
 		<div>
 			<h2>Login Form</h2>
-			<form>
+			<form onSubmit={submitHandler}>
 				<label>
 					<span>Email:</span>
 					<br />
-					<input type="email" name="email"></input>
+					<input type="test" name="email"></input>
 				</label>
 				<br />
 				<label>
